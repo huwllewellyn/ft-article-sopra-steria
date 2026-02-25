a29c7f9134fe3bedc99d965c4cd02e14ce9069d0
huw <huw.llewellyn@gmail.com>
2026-02-25 14:46:56 +0000

    Updates

--- assets/index-C6d3B11h.js
+++ assets/index-C6d3B11h.js
@@ -39,10 +39,10 @@ Error generating stack: `+c.message+`
     position: relative;
     overflow: hidden;
     ${media.tablet(`
-        min-height: calc(100lvh - 80px);
+        min-height: calc(100svh - 80px);
     `)}
     ${media.mobile(`
-        min-height: calc(100lvh - 150px);
+        min-height: calc(100svh - 150px);
     `)}
     ${media.wide(`
         min-height: calc(100lvh - 190px);
