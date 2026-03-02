d658934b3fb7ad06b100a699ec597f3edfa7894d
huw <huw.llewellyn@gmail.com>
2026-03-02 14:52:08 +0000

    Add lottie file back in

--- src/components/chapters/FinanceChapter.jsx
+++ src/components/chapters/FinanceChapter.jsx
@@ -622,11 +622,11 @@ export default function FinanceChapter() {
                     timestamp="6:30pm"
                     backgroundVideo={VIDEOS.phoneInHand}
                     poster={POSTERS.phoneInHand}
-                    // lottieOverlay={{
-                    //     mobile: "/videos/ch2/SOPRA_CH2_430pm_MOBILE.json",
-                    //     tablet: "/videos/ch2/SOPRA_CH2_430pm_MOBILE.json",
-                    //     desktop: "/videos/ch2/SOPRA_CH2_430pm_MOBILE.json",
-                    // }}
+                    lottieOverlay={{
+                        mobile: "/videos/ch2/SOPRA_CH2_430pm_MOBILE.json",
+                        tablet: "/videos/ch2/SOPRA_CH2_430pm_MOBILE.json",
+                        desktop: "/videos/ch2/SOPRA_CH2_430pm_MOBILE.json",
+                    }}
                     mobileScale={0.95}
                     textPosition="top"
                     accentColor={ACCENT}
