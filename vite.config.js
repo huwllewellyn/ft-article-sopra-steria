e310073255bc534dc5b5ce2b337286a6dff0d010
huw <huw.llewellyn@gmail.com>
2026-03-05 15:56:19 +0000

    move to article

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
     base: command === 'serve' ? '/' : '/ft-article-sopra-steria/',
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          article: resolve(__dirname, 'article.html'),
+        },
+      },
+    },
   }
 })
