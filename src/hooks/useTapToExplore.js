eea432cb6669616a4514338e5c7fbce49f49d479
huw <huw.llewellyn@gmail.com>
2026-03-05 10:38:47 +0000

    attempt android fix

--- src/hooks/useTapToExplore.js
+++ src/hooks/useTapToExplore.js
@@ -120,7 +120,7 @@ export default function useTapToExplore() {
 
             if (
                 e.target.closest(
-                    "a, button, input, select, textarea, [role='button'], video, nav, .o-header",
+                    "a, button, input, select, textarea, [role='button'], nav, .o-header",
                 )
             )
                 return;
